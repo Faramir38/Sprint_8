f6e37e74bc00855b787166ca60aca09ffb7494c6
Faramir38 <117269289+Faramir38@users.noreply.github.com>
2023-01-25 16:17:03 +0300

    First

---
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -0,0 +1,21 @@
+package ru.yandex.praktikum;
+
+public class Account {
+
+        private final String name;
+
+        public Account(String name) {
+            this.name = name;
+        }
+
+        public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
+                return (name.length() >= 3) && (name.length() <= 19) && (name.split(" ").length == 2) &&
+                        !name.startsWith(" ") && !name.endsWith(" ");
+        }
+
+}
+
